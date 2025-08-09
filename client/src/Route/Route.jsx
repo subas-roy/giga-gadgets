5dfe72967e9b9f34227777173640b115a70bb18f
subas-roy <subasroy46@gmail.com>
2025-08-09 10:37:55 +0600

    Initialized tailwind + react-router-dom

---
+++ client/src/Route/Route.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const Route = createBrowserRouter([
+    {
+        path: "/",
+        element:<div>Home</div>
+    }
+]);
+
+export default Route;
